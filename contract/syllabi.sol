03bf20ceb840b31fdda54bd8e7ddb74e30027cd6
mehmetsefa <mehmetsefa2644@gmail.com>
2018-03-12 22:01:53 +0300

    struct initialization errors are resolved

--- contract/syllabi.sol
+++ contract/syllabi.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.4.18;
 contract Syllabi {
 
     University[] universities;
+    University uniGlobal;
+    Department departmentGlobal;
+    Semester semesterGlobal;
+    Course courseGlobal;
     mapping(string => uint) uniNameToId;
 
     struct University {
@@ -29,28 +33,32 @@ contract Syllabi {
     }
 
     function addUniversity(string _name) external {
-        uint id = universities.push(University(_name, []));
+        uniGlobal.name = _name;
+        uint id = universities.push(uniGlobal);
         uniNameToId[_name] = id;
     }
 
     function addSemester(string _uniName, string _semesterName) external {
-        University memory uni = universities[uniNameToId[_uniName]];
-        uint id = uni.semesters.push(_semesterName, []);
+        University storage uni = universities[uniNameToId[_uniName]];
+        semesterGlobal.name = _semesterName;
+        uint id = uni.semesters.push(semesterGlobal);
         uni.semesterNameToId[_semesterName] = id;
     }
 
     function addDepartment(string _uniName, string _semesterName, string _departmentName) external {
-        University memory uni = universities[uniNameToId[_uniName]];
-        Semester memory semester = uni.semesters[uni.semesterNameToId[_semesterName]];
-        uint id = semester.departments.push(_departmentName, []);
-        semester.departmentNameToId[_departmentName] = id;
+        University storage uni = universities[uniNameToId[_uniName]];
+        departmentGlobal.name = _departmentName;
+        uint id = uni.semesters[uni.semesterNameToId[_semesterName]].departments.push(departmentGlobal);
+        uni.semesters[uni.semesterNameToId[_semesterName]].departmentNameToId[_departmentName] = id;
     }
 
     function addCourse(string _uniName, string _semesterName, string _departmentName, string _courseName, bytes32 _hash) external {
-        University memory uni = universities[uniNameToId[_uniName]];
-        Semester memory semester = uni.semesters[uni.semesterNameToId[_semesterName]];
-        Department memory department = semester.departments[semester.departmentNameToId[_departmentName]];
-        uint id = department.courses.push(_courseName, _hash);
+        University storage uni = universities[uniNameToId[_uniName]];
+        Semester storage semester = uni.semesters[uni.semesterNameToId[_semesterName]];
+        Department storage department = semester.departments[semester.departmentNameToId[_departmentName]];
+        courseGlobal.name = _courseName;
+        courseGlobal.fileHash = _hash;
+        uint id = department.courses.push(courseGlobal);
         department.courseNameToId[_courseName] = id;
     }
 }
